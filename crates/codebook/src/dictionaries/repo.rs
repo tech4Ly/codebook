d8b31a049acd509b7e60a5c2ee74aed2db5c2599
Bo Lopker <lopkerk@gmail.com>
2025-04-23 13:29:11 -0700

    Use wooorm for Swedish

--- crates/codebook/src/dictionaries/repo.rs
+++ crates/codebook/src/dictionaries/repo.rs
@@ -96,8 +96,8 @@ static HUNSPELL_DICTIONARIES: LazyLock<Vec<HunspellRepo>> = LazyLock::new(|| {
         ),
         HunspellRepo::new(
             "sv",
-            "https://raw.githubusercontent.com/titoBouzout/Dictionaries/refs/heads/master/Swedish.aff",
-            "https://raw.githubusercontent.com/titoBouzout/Dictionaries/refs/heads/master/Swedish.dic",
+            "https://raw.githubusercontent.com/wooorm/dictionaries/refs/heads/main/dictionaries/sv/index.aff",
+            "https://raw.githubusercontent.com/wooorm/dictionaries/refs/heads/main/dictionaries/sv/index.dic",
         ),
     ]
 });
