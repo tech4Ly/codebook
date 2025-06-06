7046d4ca4c8bd53484565b7127084a30f0383950
Bo Lopker <lopkerk@gmail.com>
2025-06-05 23:43:42 -0700

    Add a benchmark argument to the main codebook bin

--- crates/codebook/src/main.rs
+++ crates/codebook/src/main.rs
@@ -1,6 +1,7 @@
 use codebook::Codebook;
 use codebook::queries::LanguageType;
 use std::env;
+use std::fs::File;
 use std::path::Path;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
@@ -42,27 +43,34 @@ fn main() {
     println!("Done");
 }
 
+#[cfg(target_os = "windows")]
+fn run_benchmark(processor: &Codebook) {
+    println!("Not supported in windows");
+}
+
+#[cfg(not(target_os = "windows"))]
 fn run_benchmark(processor: &Codebook) {
     println!("Running spell_check benchmark...");
-
+    let guard = pprof::ProfilerGuardBuilder::default()
+        .frequency(998)
+        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
+        .build()
+        .unwrap();
     // Define sample text for benchmark
-    let sample_text = r#"
-        fn calculate_user_age(bithDate: String) -> u32 {
-            // This is a codebook example_function that calculates age
-            let userAge = get_current_date() - birthDate;
-            userAge
-        }
-    "#;
+    let sample_text = include_str!("../../../examples/wulf.txt");
 
     // Number of iterations for benchmark
-    let iterations = 1000;
+    let iterations = 100;
 
     // Start timing
     let start = Instant::now();
 
     // Run spell_check multiple times
-    for _ in 1..=iterations {
-        let _misspelled = processor.spell_check(sample_text, Some(LanguageType::Rust), None);
+    for i in 1..=iterations {
+        if i % 10 == 0 {
+            println!("Iteration {}/{}", i, iterations);
+        }
+        let _misspelled = processor.spell_check(sample_text, Some(LanguageType::Text), None);
     }
 
     // Calculate duration
@@ -76,4 +84,8 @@ fn run_benchmark(processor: &Codebook) {
         "Average time per iteration: {:.6?}",
         Duration::from_secs_f64(avg_time)
     );
+    if let Ok(report) = guard.report().build() {
+        let file = File::create("flamegraph.svg").unwrap();
+        report.flamegraph(file).unwrap();
+    };
 }
